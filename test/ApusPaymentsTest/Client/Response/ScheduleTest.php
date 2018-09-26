3e3e29430091d04e05e178be039193f6de0cd7a0
Marcos Rocha <marcos.renan@gmail.com>
2018-09-26 16:06:49 -0300

    Adicionado updateValues para Schedule (+teste)

--- test/ApusPaymentsTest/Client/Response/ScheduleTest.php
+++ test/ApusPaymentsTest/Client/Response/ScheduleTest.php
@@ -2,10 +2,23 @@
 namespace ApusPaymentsTest\Client\Response;
 
 use PHPUnit\Framework\TestCase;
+use ApusPayments\Client\Response\Schedule;
 
 class ScheduleTest extends TestCase {
     
     public function testUpdateValues() {
+        $data = new \stdClass();
+        $data->execute = true;
+        $data->frequency = 12;
+        $data->id = "string";
+        $data->period = "string-period";
         
+        $schedule = new Schedule();
+        $schedule->updateValues($data);
+        
+        $this->assertEquals(true, $schedule->isExecute());
+        $this->assertEquals(12, $schedule->getFrequency());
+        $this->assertEquals("string", $schedule->getId());
+        $this->assertEquals("string-period", $schedule->getPeriod());
     }
 }
